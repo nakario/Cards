a7b41ae81dcff58e000fdf985228c5809cf7e980
nakario <nakario.jp@gmail.com>
2017-02-21 11:33:40 +0900

    Files and Cards

---
+++ src/components/File.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="file">
+    <card></card>
+  </div>
+</template>
+
+<script>
+import Card from './Card';
+
+export default {
+  name: 'file',
+  data: function() {
+    return {
+      cards: [],
+    };
+  },
+  components: {
+    'card': Card,
+  },
+};
+</script>
+
+<style scoped>
+</style>
