fb6712de8d2c1778bd6cf9b9fed4c521f7e63954
nakario <nakario.jp@gmail.com>
2017-03-11 17:14:25 +0900

    Make table from props

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,40 +1,72 @@
 <template>
-  <table>
-    <thead>
-      <th></th>
-      <td>(1)</td>
-      <td>(2)</td>
-      <td>(3)</td>
-    </thead>
-    <tbody>
-      <tr>
-        <th>(1)</th>
-        <td>
-          <div class="placeholder" contenteditable="true"></div>
+  <div class="table-wrapper">
+    <table>
+      <tr v-for="row in rows">
+        <td v-for="col in cols">
+          <div class="placeholder" @blur="log(`${row}, ${col}`)" contenteditable="true">
+            {{ td[row][col].type == undefined ? 'x' : td[row][col].data }}
+          </div>
         </td>
-        <td></td>
-        <td></td>
       </tr>
-      <tr>
-        <th>(2)</th>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
-      <tr>
-        <th>(3)</th>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
-    </tbody>
-  </table>
+    </table>
+  </div>
 </template>
 
 <script>
+  const MAX_ROW = 5;
+  const MAX_COL = 10;
+
+  function hoge() {
+    return [[{ data: 'a', type: 'String' }, { data: 'a', type: 'String' }, { data: 'a', type: 'String' }], [{ data: 'a', type: 'String' }, { data: 'a', type: 'String' }, { data: 'a', type: 'String' }], [{ data: 'a', type: 'String' }, { data: 'a', type: 'String' }, { data: 'a', type: 'String' }], [{ data: 'a', type: 'String' }, { data: 'a', type: 'String' }, { data: 'a', type: 'String' }]];
+  }
+
+  export default {
+    props: {
+      row: {
+        default: 2,
+      },
+      col: {
+        default: 2,
+      },
+      table: {
+        default: hoge,
+      },
+    },
+    computed: {
+      rows: function() {
+        return [...Array(MAX_ROW + 1).keys()];
+      },
+      cols: function() {
+        return [...Array(MAX_COL + 1).keys()];
+      },
+      td: function() {
+        const rows = [];
+        for (const i of this.rows) {
+          const cols = [];
+          for (const j of this.cols) {
+            if (i <= this.row && j <= this.col) {
+              cols.push({ data: this.table[i][j].data, type: 'String' });
+            } else {
+              cols.push({ data: undefined, type: undefined });
+            }
+          }
+          rows.push(cols);
+        }
+        return rows;
+      },
+    },
+    methods: {
+      log: function(msg) {
+        console.log(msg);
+      },
+    },
+  };
 </script>
 
 <style scoped>
+  .tabel-wrapper {
+    overflow: hidden;
+  }
   table {
     border-collapse: collapse;
   }
