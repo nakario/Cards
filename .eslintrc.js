5050b03246dcadad5c36af62d7e59925e7c1392d
nakario <nakario.jp@gmail.com>
2017-03-11 17:12:16 +0900

    Arrow 'for-of-statement' & 'console.log()'

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,6 +39,8 @@ module.exports = {
     'object-shorthand': 0,
     'func-names': 0,
     'space-before-function-paren': 0,
-    'quote-props': 0
+    'quote-props': 0,
+    'no-restricted-syntax': 0,
+    'no-console': 0
   }
 }
